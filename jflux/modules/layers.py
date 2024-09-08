d894b3a936572f290b203a763f8fef9d6defd5b9
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-09-08 16:14:24 +0100

    fix(modules/layers): correct invocation of nnx modules

--- jflux/modules/layers.py
+++ jflux/modules/layers.py
@@ -1,19 +1,30 @@
 import math
 from dataclasses import dataclass
 
-from jax import (
-    Array,
-    numpy as jnp,
-)
+import jax
+import jax.numpy as jnp
+from jax import Array
 from einops import rearrange
 from flax import nnx
+from functools import partial
 
 from jflux.math import attention, rope
 
 
-class EmbedND(nnx.Module):
+class Embed(nnx.Module):
+    """
+    Embedding module for Positional Embeddings.
+
+    Args:
+        dim (int): Dimension of the embedding.
+        theta (int): theta parameter for the RoPE embedding
+        axes_dim (list[int]): List of axes dimensions.
+
+    Returns:
+        RoPE embeddings
+    """
+
     def __init__(self, dim: int, theta: int, axes_dim: list[int]) -> None:
-        super().__init__()
         self.dim = dim
         self.theta = theta
         self.axes_dim = axes_dim
@@ -22,80 +33,91 @@ class EmbedND(nnx.Module):
         n_axes = ids.shape[-1]
         emb = jnp.concat(
             [rope(ids[..., i], self.axes_dim[i], self.theta) for i in range(n_axes)],
-            dim=-3,
+            axis=-3,
         )
 
         return jnp.expand_dims(emb, 1)
 
 
-def timestep_embedding(t: Array, dim, max_period=10000, time_factor: float = 1000.0):
+@partial(jax.jit, static_argnums=(1, 2, 3))
+def timestep_embedding(
+    t: Array, dim: int, max_period=10000, time_factor: float = 1000.0
+) -> Array:
     """
-    Create sinusoidal timestep embeddings.
-    :param t: a 1-D Array of N indices, one per batch element.
-                      These may be fractional.
-    :param dim: the dimension of the output.
-    :param max_period: controls the minimum frequency of the embeddings.
-    :return: an (N, D) Array of positional embeddings.
+    Generate timestep embeddings.
+
+    Args:
+        t (Array): An array of timesteps to be embedded.
+        dim (int): The desired dimensionality of the output embedding.
+        max_period (int, optional): The maximum period for the sinusoidal functions. Defaults to 10000.
+        time_factor (float, optional): A scaling factor applied to the input timesteps. Defaults to 1000.0.
+
+    Returns:
+        timestep embeddings.
     """
+    # Pre-Processing:
+    # * scales the input timesteps by the given time factor
     t = time_factor * t
     half = dim // 2
-    freqs = jnp.exp(
-        -math.log(max_period) * jnp.arange(start=0, end=half, dtype=jnp.float32) / half
-    ).to(t.device)
 
-    args = t[:, None].float() * freqs[None]
+    # Determine frequencies using exponential decay
+    freqs = jnp.exp(
+        -math.log(max_period)
+        * jnp.arange(start=0, stop=half, dtype=jnp.float32, device=t.device())
+        / half
+    )
+
+    # Create embeddings by concatenating sine and cosines
+    args = t[:, None].astype(jnp.float32) * freqs[None]
     embedding = jnp.concat([jnp.cos(args), jnp.sin(args)], axis=-1)
+
+    # Handle odd dimensions
     if dim % 2:
         embedding = jnp.concat([embedding, jnp.zeros_like(embedding[:, :1])], axis=-1)
-    if torch.is_floating_point(t):
-        embedding = embedding.to(t)
+
+    # If timestamps are floating types ensure so is the embedding
+    if jnp.issubdtype(t.device(), jnp.floating):
+        embedding = embedding.astype(t.device())
+
     return embedding
 
 
 class MLPEmbedder(nnx.Module):
-    def __init__(self, in_dim: int, hidden_dim: int):
-        super().__init__()
-        self.in_layer = nnx.Linear(in_dim, hidden_dim, bias=True)
-        self.silu = nnx.silu
-        self.out_layer = nnx.Linear(hidden_dim, hidden_dim, bias=True)
+    def __init__(self, in_dim: int, hidden_dim: int, rngs: nnx.Rngs) -> None:
+        self.in_layer = nnx.Linear(
+            in_features=in_dim, out_features=hidden_dim, use_bias=True, rngs=rngs
+        )
+        self.out_layer = nnx.Linear(
+            in_features=in_dim, out_features=hidden_dim, use_bias=True, rngs=rngs
+        )
 
     def __call__(self, x: Array) -> Array:
-        return self.out_layer(self.silu(self.in_layer(x)))
-
-
-class RMSNorm(nnx.Module):
-    def __init__(self, dim: int):
-        super().__init__()
-        self.scale = nnx.Param(jnp.ones(dim))
-
-    def __call__(self, x: Array):
-        x_dtype = x.dtype
-        x = x.astype(jnp.float32)
-        rrms = jnp.reciprocal(jnp.sqrt(jnp.mean(x**2, axis=-1, keepdim=True) + 1e-6))
-        return (x * rrms).astype(x_dtype) * self.scale
+        return self.out_layer(nnx.silu(self.in_layer(x)))
 
 
 class QKNorm(nnx.Module):
-    def __init__(self, dim: int):
-        super().__init__()
-        self.query_norm = RMSNorm(dim)
-        self.key_norm = RMSNorm(dim)
+    def __init__(self, dim: int, rngs: nnx.Rngs) -> None:
+        self.query_norm = nnx.RMSNorm(dim, rngs=rngs)
+        self.key_norm = nnx.RMSNorm(dim, rngs=rngs)
 
     def __call__(self, q: Array, k: Array, v: Array) -> tuple[Array, Array]:
         q = self.query_norm(q)
         k = self.key_norm(k)
-        return q.to(v), k.to(v)
+        return q.astype(v.device()), k.astype(v.device())
 
 
 class SelfAttention(nnx.Module):
-    def __init__(self, dim: int, num_heads: int = 8, qkv_bias: bool = False):
-        super().__init__()
+    def __init__(
+        self, dim: int, rngs: nnx.Rngs, num_heads: int = 8, qkv_bias: bool = False
+    ) -> None:
         self.num_heads = num_heads
         head_dim = dim // num_heads
 
-        self.qkv = nnx.Linear(dim, dim * 3, bias=qkv_bias)
-        self.norm = QKNorm(head_dim)
-        self.proj = nnx.Linear(dim, dim)
+        self.qkv = nnx.Linear(
+            in_features=dim, out_features=dim * 3, use_bias=qkv_bias, rngs=rngs
+        )
+        self.norm = QKNorm(head_dim, rngs=rngs)
+        self.proj = nnx.Linear(in_features=dim, out_features=dim, rngs=rngs)
 
     def __call__(self, x: Array, pe: Array) -> Array:
         qkv = self.qkv(x)
@@ -114,16 +136,14 @@ class ModulationOut:
 
 
 class Modulation(nnx.Module):
-    def __init__(self, dim: int, double: bool):
-        super().__init__()
+    def __init__(self, dim: int, double: bool, rngs: nnx.Rngs) -> None:
         self.is_double = double
         self.multiplier = 6 if double else 3
-        self.lin = nnx.Linear(dim, self.multiplier * dim, bias=True)
+        self.lin = nnx.Linear(dim, self.multiplier * dim, use_bias=True, rngs=rngs)
 
     def __call__(self, vec: Array) -> tuple[ModulationOut, ModulationOut | None]:
-        out = self.lin(nnx.functional.silu(vec))[:, None, :].chunk(
-            self.multiplier, dim=-1
-        )
+        ary = self.lin(nnx.silu(vec))[:, None, :]
+        out = jnp.split(ary, self.multiplier, axis=-1)
 
         return (
             ModulationOut(*out[:3]),
@@ -133,37 +153,42 @@ class Modulation(nnx.Module):
 
 class DoubleStreamBlock(nnx.Module):
     def __init__(
-        self, hidden_size: int, num_heads: int, mlp_ratio: float, qkv_bias: bool = False
+        self,
+        hidden_size: int,
+        num_heads: int,
+        mlp_ratio: float,
+        rngs: nnx.Rngs,
+        qkv_bias: bool = False,
     ):
         super().__init__()
 
         mlp_hidden_dim = int(hidden_size * mlp_ratio)
         self.num_heads = num_heads
         self.hidden_size = hidden_size
-        self.img_mod = Modulation(hidden_size, double=True)
-        self.img_norm1 = nnx.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.img_mod = Modulation(hidden_size, double=True, rngs=rngs)
+        self.img_norm1 = nnx.LayerNorm(hidden_size, epsilon=1e-6, rngs=rngs)
         self.img_attn = SelfAttention(
-            dim=hidden_size, num_heads=num_heads, qkv_bias=qkv_bias
+            dim=hidden_size, num_heads=num_heads, qkv_bias=qkv_bias, rngs=rngs
         )
 
-        self.img_norm2 = nnx.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.img_norm2 = nnx.LayerNorm(hidden_size, epsilon=1e-6, rngs=rngs)
         self.img_mlp = nnx.Sequential(
-            nnx.Linear(hidden_size, mlp_hidden_dim, bias=True),
-            nnx.GELU(approximate="tanh"),
-            nnx.Linear(mlp_hidden_dim, hidden_size, bias=True),
+            nnx.Linear(hidden_size, mlp_hidden_dim, use_bias=True, rngs=rngs),
+            nnx.gelu,
+            nnx.Linear(mlp_hidden_dim, hidden_size, use_bias=True, rngs=rngs),
         )
 
-        self.txt_mod = Modulation(hidden_size, double=True)
-        self.txt_norm1 = nnx.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.txt_mod = Modulation(hidden_size, double=True, rngs=rngs)
+        self.txt_norm1 = nnx.LayerNorm(hidden_size, epsilon=1e-6, rngs=rngs)
         self.txt_attn = SelfAttention(
-            dim=hidden_size, num_heads=num_heads, qkv_bias=qkv_bias
+            dim=hidden_size, num_heads=num_heads, qkv_bias=qkv_bias, rngs=rngs
         )
 
-        self.txt_norm2 = nnx.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.txt_norm2 = nnx.LayerNorm(hidden_size, epsilon=1e-6, rngs=rngs)
         self.txt_mlp = nnx.Sequential(
-            nnx.Linear(hidden_size, mlp_hidden_dim, bias=True),
-            nnx.GELU(approximate="tanh"),
-            nnx.Linear(mlp_hidden_dim, hidden_size, bias=True),
+            nnx.Linear(hidden_size, mlp_hidden_dim, use_bias=True, rngs=rngs),
+            nnx.gelu,
+            nnx.Linear(mlp_hidden_dim, hidden_size, use_bias=True, rngs=rngs),
         )
 
     def __call__(
@@ -191,9 +216,9 @@ class DoubleStreamBlock(nnx.Module):
         txt_q, txt_k = self.txt_attn.norm(txt_q, txt_k, txt_v)
 
         # run actual attention
-        q = jnp.concat((txt_q, img_q), dim=2)
-        k = jnp.concat((txt_k, img_k), dim=2)
-        v = jnp.concat((txt_v, img_v), dim=2)
+        q = jnp.concat((txt_q, img_q), axis=2)
+        k = jnp.concat((txt_k, img_k), axis=2)
+        v = jnp.concat((txt_v, img_v), axis=2)
 
         attn = attention(q, k, v, pe=pe)
         txt_attn, img_attn = attn[:, : txt.shape[1]], attn[:, txt.shape[1] :]
@@ -222,10 +247,10 @@ class SingleStreamBlock(nnx.Module):
         self,
         hidden_size: int,
         num_heads: int,
+        rngs: nnx.Rngs,
         mlp_ratio: float = 4.0,
         qk_scale: float | None = None,
     ):
-        super().__init__()
         self.hidden_dim = hidden_size
         self.num_heads = num_heads
         head_dim = hidden_size // num_heads
@@ -233,23 +258,27 @@ class SingleStreamBlock(nnx.Module):
 
         self.mlp_hidden_dim = int(hidden_size * mlp_ratio)
         # qkv and mlp_in
-        self.linear1 = nnx.Linear(hidden_size, hidden_size * 3 + self.mlp_hidden_dim)
+        self.linear1 = nnx.Linear(
+            hidden_size, hidden_size * 3 + self.mlp_hidden_dim, rngs=rngs
+        )
         # proj and mlp_out
-        self.linear2 = nnx.Linear(hidden_size + self.mlp_hidden_dim, hidden_size)
+        self.linear2 = nnx.Linear(
+            hidden_size + self.mlp_hidden_dim, hidden_size, rngs=rngs
+        )
 
-        self.norm = QKNorm(head_dim)
+        self.norm = QKNorm(head_dim, rngs=rngs)
 
         self.hidden_size = hidden_size
-        self.pre_norm = nnx.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.pre_norm = nnx.LayerNorm(hidden_size, epsilon=1e-6, rngs=rngs)
 
-        self.mlp_act = nnx.GELU(approximate="tanh")
-        self.modulation = Modulation(hidden_size, double=False)
+        self.mlp_act = nnx.gelu
+        self.modulation = Modulation(hidden_size, double=False, rngs=rngs)
 
     def __call__(self, x: Array, vec: Array, pe: Array) -> Array:
         mod, _ = self.modulation(vec)
         x_mod = (1 + mod.scale) * self.pre_norm(x) + mod.shift
         qkv, mlp = jnp.split(
-            self.linear1(x_mod), [3 * self.hidden_size, self.mlp_hidden_dim], dim=-1
+            self.linear1(x_mod), [3 * self.hidden_size, self.mlp_hidden_dim], axis=-1
         )
 
         q, k, v = rearrange(qkv, "B L (K H D) -> K B H L D", K=3, H=self.num_heads)
@@ -258,19 +287,24 @@ class SingleStreamBlock(nnx.Module):
         # compute attention
         attn = attention(q, k, v, pe=pe)
         # compute activation in mlp stream, cat again and run second linear layer
-        output = self.linear2(jnp.concat((attn, self.mlp_act(mlp)), 2))
+        output = self.linear2(jnp.concat((attn, self.mlp_act(mlp)), axis=2))
         return x + mod.gate * output
 
 
 class LastLayer(nnx.Module):
-    def __init__(self, hidden_size: int, patch_size: int, out_channels: int):
+    def __init__(
+        self, hidden_size: int, patch_size: int, out_channels: int, rngs: nnx.Rngs
+    ) -> None:
         super().__init__()
-        self.norm_final = nnx.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.norm_final = nnx.LayerNorm(hidden_size, epsilon=1e-6, rngs=rngs)
         self.linear = nnx.Linear(
-            hidden_size, patch_size * patch_size * out_channels, bias=True
+            hidden_size,
+            patch_size * patch_size * out_channels,
+            use_bias=True,
+            rngs=rngs,
         )
         self.adaLN_modulation = nnx.Sequential(
-            nnx.SiLU(), nnx.Linear(hidden_size, 2 * hidden_size, bias=True)
+            nnx.silu, nnx.Linear(hidden_size, 2 * hidden_size, use_bias=True, rngs=rngs)
         )
 
     def __call__(self, x: Array, vec: Array) -> Array:
